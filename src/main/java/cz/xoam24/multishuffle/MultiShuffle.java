7e8f063707a4f20d291551a0fc7832eb21e1aa34
xoam24 <infoxoam24@gmail.com>
2026-03-14 18:09:53 +0100

    init commit

---
+++ src/main/java/cz/xoam24/multishuffle/MultiShuffle.java
@@ -0,0 +1,17 @@
+package cz.xoam24.multishuffle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MultiShuffle extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
